411432dc101c881775d17cdfd783feee1b4acb9d
Daniel Standage <daniel.standage@gmail.com>
2016-01-07 22:39:35 -0500

    Update Travis config: GenomeTools 1.5.8 dependency

--- scripts/travis-prereqs.sh
+++ scripts/travis-prereqs.sh
@@ -6,13 +6,13 @@ mkdir -p $HOME/local/src $HOME/local/bin $HOME/local/lib $HOME/local/include
 
 if [ ! -f "$HOME/local/tgz/master.tar.gz" ]; then
     mkdir -p $HOME/local/tgz
-    wget -P $HOME/local/tgz http://genometools.org/pub/binary_distributions/gt-1.5.7-Linux_x86_64-64bit-complete.tar.gz
+    wget -P $HOME/local/tgz http://genometools.org/pub/binary_distributions/gt-1.5.8-Linux_x86_64-64bit-complete.tar.gz
     wget -P $HOME/local/tgz https://github.com/standage/AEGeAn/archive/master.tar.gz
 fi
 
 # Install GenomeTools
-gtdir="$HOME/local/src/gt-1.5.7-Linux_x86_64-64bit-complete"
-tar -xzf $HOME/local/tgz/gt-1.5.7-Linux_x86_64-64bit-complete.tar.gz --directory $HOME/local/src
+gtdir="$HOME/local/src/gt-1.5.8-Linux_x86_64-64bit-complete"
+tar -xzf $HOME/local/tgz/gt-1.5.8-Linux_x86_64-64bit-complete.tar.gz --directory $HOME/local/src
 cp -r $gtdir/bin/* $HOME/local/bin/
 cp -r $gtdir/include/genometools $HOME/local/include/
 cp -r $gtdir/lib/* $HOME/local/lib/
