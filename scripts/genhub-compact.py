debe19f16d12b1553591aa29a7e4de1914a279fb
Daniel Standage <daniel.standage@gmail.com>
2016-04-05 20:12:52 -0400

    Improved compactness script, filtering implemented

--- scripts/genhub-compact.py
+++ scripts/genhub-compact.py
@@ -11,6 +11,7 @@
 from __future__ import print_function
 from __future__ import division
 import argparse
+import pandas
 import re
 import sys
 import genhub
@@ -18,7 +19,7 @@ import genhub
 
 def cli():
     """Define the command-line interface of the program."""
-    desc = 'Calculate measures of "compactness" for the specified genome(s).'
+    desc = 'Calculate measures of compactness for the specified genome(s).'
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-v', '--version', action='version',
                         version='GenHub v%s' % genhub.__version__)
@@ -36,65 +37,47 @@ def cli():
     return parser
 
 
-def ilocus_type(record):
-    typematch = re.search('iLocus_type=([^;\n]+)', record)
-    assert typematch
-    return typematch.group(1)
+def thresholds(seqid, iloci, iqnt=0.95, gqnt=0.05):
+    seqloci = iloci.loc[iloci.SeqID == seqid]
+    ithresh = None
+    if iqnt:
+        iiloci = seqloci.loc[seqloci.LocusClass == 'iiLocus']
+        ithresh = int(iiloci['Length'].quantile(iqnt))
+    gthresh = None
+    if gqnt:
+        gilocus_types = ['siLocus', 'ciLocus', 'niLocus']
+        giloci = seqloci.loc[seqloci.LocusClass.isin(gilocus_types)]
+        gthresh = int(giloci['Length'].quantile(iqnt))
+    return ithresh, gthresh
 
 
-def seq_info(db):
-    with open(db.ilocusfile) as infile:
-        seqlengths = dict()
-        gilocus_counts = dict()
-        for line in infile:
-            if line.startswith('##sequence-region'):
-                seqreg = re.search('##sequence-region\s+(\S+)\s+(\d+)\s+(\d+)',
-                                   line)
-                assert seqreg
-                seqid = seqreg.group(1)
-                start = int(seqreg.group(2))
-                end = int(seqreg.group(3))
-                assert start == 1
-                seqlengths[seqid] = end
-            elif '\tlocus\t' in line:
-                values = line.split('\t')
-                assert len(values) == 9
-                assert values[2] == 'locus'
-                seqid = values[0]
-                attrs = values[8]
-                locustype = ilocus_type(attrs)
-                if locustype in ['siLocus', 'ciLocus', 'niLocus']:
-                    if seqid not in gilocus_counts:
-                        gilocus_counts[seqid] = 0
-                    gilocus_counts[seqid] += 1
-
-    return seqlengths, gilocus_counts
+def seqlen(seqid, iloci, ithresh=None, gthresh=None):
+    seqloci = iloci.loc[(iloci.SeqID == seqid) &
+                        (seqloci.LocusClass != 'fiLocus')]
+    effsize = seqloci['EffectiveLength'].sum()
+    if ithresh:
+        longiiloci = seqloci.loc[(seqloci.LocusClass == 'iiLocus') &
+                                 (seqloci.Length > ithresh)]
+        effsize -= longiiloci['Length'].sum()
+    if gthresh:
+        gilocus_types = ['siLocus', 'ciLocus', 'niLocus']
+        shortgiloci = seqloci.loc[(seqloci.LocusClass.isin(gilocus_types)) &
+                                  (seqloci.Length < gthresh)]
+        effsize -= shortgiloci['Length'].sum()
+    return effsize
 
 
-def milocus_info(db):
-    with open(db.milocusfile) as infile:
-        milocus_occ = dict()
-        singleton_count = dict()
-        for line in infile:
-            values = line.split('\t')
-            assert len(values) == 9
-            assert values[2] == 'locus'
-            seqid = values[0]
-            start = int(values[3]) - 1
-            end = int(values[4])
-            attrs = values[8]
-            locustype = ilocus_type(attrs)
-            if locustype == 'miLocus':
-                length = end - start
-                if seqid not in milocus_occ:
-                    milocus_occ[seqid] = 0
-                milocus_occ[seqid] += length
-            elif locustype in ['siLocus', 'ciLocus', 'niLocus']:
-                if seqid not in singleton_count:
-                    singleton_count[seqid] = 0
-                singleton_count[seqid] += 1
-    print(milocus_occ)
-    return milocus_occ, singleton_count
+def calc_phi(seqid, iloci, miloci):
+    gilocus_types = ['siLocus', 'ciLocus', 'niLocus']
+    giloci = iloci.loc[(iloci.SeqID == seqid) &
+                       (iloci.LocusClass.isin(gilocus_types))]
+    singletons = miloci.loc[(miloci.SeqID == seqid) &
+                            (miloci.LocusClass.isin(gilocus_types))]
+    if gthresh:
+        giloci = giloci.loc[giloci.Length >= gthresh]
+        singletons = singletons.loc[singletons.Length >= gthresh]
+    merged = len(giloci) - len(singletons)
+    return merged / len(giloci)
 
 
 def main(args):
@@ -109,23 +92,14 @@ def main(args):
     for species in args.species:
         config = conf[species]
         db = genhub.genomedb.GenomeDB(species, config, workdir=args.workdir)
-        seqlengths, gilocus_counts = seq_info(db)
-        milocus_occ, singleton_count = milocus_info(db)
-        for seqid in seqlengths:
-            length = seqlengths[seqid]
-            if length < args.length:
-                continue
-            milocus_space = 0
-            if seqid in milocus_occ:
-                milocus_space = milocus_occ[seqid]
-            giloci = gilocus_counts[seqid]
-            singletons = 0
-            if seqid in singleton_count:
-                singletons = singleton_count[seqid]
-            nonsingletons = giloci - singletons
-            sigma = milocus_space / length
-            phi = nonsingletons / giloci
-
+        iloci = pandas.read_table(db.ilocustable)
+        miloci = pandas.read_table(db.milocustable)
+        for seqid in iloci['SeqID']:
+            ithresh, gthresh = thresholds(seqid, iloci)
+            length = seqlen(seqid, iloci, ithresh, gthresh)
+            phi = calc_phi(seqid, iloci, miloci)
+            milocus_occ = miloci.loc[miloci.SeqID == seqid]['Length'].sum()
+            sigma = milocus_occ / length
             print(species, seqid, sigma, phi, sep='\t')
 
 if __name__ == '__main__':
